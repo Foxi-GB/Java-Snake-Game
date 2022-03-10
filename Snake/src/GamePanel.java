b5058ff07f5484f407f0ad2d724130e11e2c16a2
Foxi-GB <itsShibs@gmail.com>
2022-03-10 13:23:17 +0000

    Adding Scoreboard to Game Over
    Adding the score board to the game over screen by calling the same function when the game over screen is drawn.

--- Snake/src/GamePanel.java
+++ Snake/src/GamePanel.java
@@ -159,6 +159,11 @@ public class GamePanel extends JPanel implements ActionListener{
         g.setFont(new Font("Ink Free", Font.BOLD, 75));
         FontMetrics metrics = getFontMetrics(g.getFont()); 
         g.drawString("Game Over", (SCREEN_WIDTH - metrics.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
+        //Game Over ScoreBoard
+        g.setColor(Color.red);
+        g.setFont(new Font("Ink Free", Font.BOLD, 40));
+        FontMetrics metrics2 = getFontMetrics(g.getFont()); 
+        g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics2.stringWidth("Score: " + applesEaten))/2, g.getFont().getSize());
     }
 
     @Override
