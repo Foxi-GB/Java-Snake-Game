9d963c6bc69ffd13a02a17737f7d27e79b5ebdd9
Foxi-GB <itsShibs@gmail.com>
2022-03-10 12:35:04 +0000

    Add Necessary Variables and more Methods
    Creating the list of required variables and adding more methods that will use them.

--- Snake/src/GamePanel.java
+++ Snake/src/GamePanel.java
@@ -1,13 +1,67 @@
 import javax.swing.JPanel;
-import java.awt.event.ActionListener; 
-import java.awt.event.ActionEvent; 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener{
 
+    //variable declaration
+        //SCREEN dimensions
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+        //Object Size
+    static final int UNIT_SIZE = 25; 
+        //Number of objects in screen
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT / UNIT_SIZE); 
+        //Time delay
+    static final int DELAY = 75; 
+        //X and Y co-ordinates
+    final int x[] = new int[GAME_UNITS]; 
+    final int y[] = new int[GAME_UNITS]; 
+        //Staring body parts
+    int bodyParts = 6;
+    int applesEaten;
+        //Apple Co-Ordinates
+    int appleX; 
+    int appleY;
+        //Snake Direction
+    char direction = 'R';  
+    boolean running = false; 
+    Timer timer;
+    Random random;
+
     GamePanel(){
-        
+
     }
 
+    public void startGame(){
+
+    }
+
+    public void paintComponent(Graphics g){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void checkApple(){
+
+    }
+
+    public void checkCollisions(){
+
+    }
+
+    public void gameOver(Graphics g){
+
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -15,4 +69,11 @@ public class GamePanel extends JPanel implements ActionListener{
         
     }
     
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+    }
+
 }
